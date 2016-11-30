dab5bb98ea746447ee8aefedfdfa1927a265e238
jenn083 <nguyenj1203@gmail.com>
2016-11-29 20:33:29 -0800

    added font awesome icons and profile picture

--- app.js
+++ app.js
@@ -1,37 +1,49 @@
 'use strict';
 
 
-// The questions in the guessing game must require a mix of yes/no answers, 
-// and user input must accept either y/n or yes/no responses, with either 
-// .toUpperCase() or .toLowerCase() used to validate the user 
-// input and accommodate users entering all-caps Y/N or YES/NO answers, too.
 
-var response1 = prompt('Does Sam have any cats?').toLowerCase();
-var response2 = prompt('');
-var response3 = prompt('');
-var response4 = prompt('');
-var response5 = prompt('');
-
-// if(condition is true) {
-// 	do something;
-// 	maybe do something more;
-// 	etc. until done;
-// } else {
-// 	do this other thing;
-// 	and maybe yet another thing;
-// }
+var response1 = prompt('Does Jennifer believe in magic?').toLowerCase();
+var response2 = prompt('Is Jennifer the youngest in her family?').toLowerCase();
+var response3 = prompt('Does Jennifer know how to swim?').toLowerCase();
+var response4 = prompt('Has Jennifer ever been to Alaska?').toLowerCase();
+var response5 = prompt('Can Jennifer speak Mandarine?').toLowerCase();
 
 if(response1 === 'yes' || response1 === 'y') {
-	alert('Yes, Sam does have three cats!');
-} else if(response === 'no' || response1 === 'n') {
-	alert('Sorry, you\'re wrong, Sam has three cats');
+  alert('Yes, Jennifer does believe in magic!');
+} else if(response1 === 'no' || response1 === 'n') {
+  alert('Sorry, you\'re wrong, Jennifer does believe in magic.');
 } else {
-	alert('Try to answer with a yes/y or no/n next time.');
+  alert('Try to answer with a yes/y or no/n next time.');
 }
 
+if(response2 === 'yes' || response2 === 'y') {
+  alert('Yes, you\'re correct, Jennifer is the youngest of four children!');
+} else if(response2 === 'no' || response2 === 'n') {
+  alert('Sorry, you\'re wrong, Jennifer is the youngest of four children.');
+} else {
+  alert('Try to answer with a yes/y or no/n next time.');
+}
 
+if(response3 === 'yes' || response3 === 'y') {
+  alert('Yes, Jennifer is an excellent swimmer!');
+} else if(response3 === 'no' || response3 === 'n') {
+  alert('Sorry, you\'re wrong, Jennifer swims very well.');
+} else {
+  alert('Try to answer with a yes/y or no/n next time.');
+}
 
+if(response4 === 'yes' || response4 === 'y') {
+  alert('That is incorrect! Jennifer has never been to Alaska');
+} else if(response4 === 'no' || response4 === 'n') {
+  alert('You\'re correct! Jennifer doesn\'t like the cold weather.');
+} else {
+  alert('Try to answer with a yes/y or no/n next time.');
+}
 
-
-
-
+if(response5 === 'yes' || response5 === 'y') {
+  alert('You\'re incorrect. Jennifer does not speak Mandarine');
+} else if(response5 === 'no' || response5 === 'n') {
+  alert('Correct! Jennifer only speaks English and Vietnamese');
+} else {
+  alert('Try to answer with a yes/y or no/n next time.');
+}
