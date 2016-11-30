db88bf11c67c35c3d2dccbb22e5880434e4c8015
jenn083 <nguyenj1203@gmail.com>
2016-11-30 08:38:26 -0800

    added document.write in app.js file

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ if(response1 === 'yes' || response1 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 console.log('Does Jennifer believe in magic? User answered: '+ response1);
+document.write('You answered: ' + response1 + ' to question #1');
 var response2 = prompt('Is Jennifer the youngest in her family?').toLowerCase();
 
 if(response2 === 'yes' || response2 === 'y') {
@@ -22,6 +23,7 @@ if(response2 === 'yes' || response2 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 console.log('Is Jennifer the youngest in her family? User answered: ' + response2);
+document.write('You answered: ' + response2 + ' to question #2');
 var response3 = prompt('Does Jennifer know how to swim?').toLowerCase();
 
 if(response3 === 'yes' || response3 === 'y') {
@@ -32,6 +34,8 @@ if(response3 === 'yes' || response3 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 console.log('Does Jennifer know how to swim? User answered: ' + response3);
+document.write('You answered: ' + response3 + ' to question #3');
+
 var response4 = prompt('Has Jennifer ever been to Alaska?').toLowerCase();
 
 if(response4 === 'yes' || response4 === 'y') {
@@ -42,6 +46,8 @@ if(response4 === 'yes' || response4 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 console.log('Has Jennifer ever been to Alaska? User answered: ' + response4);
+document.write('You answered: ' + response4 + ' to question #4');
+
 var response5 = prompt('Can Jennifer speak Mandarine?').toLowerCase();
 
 if(response5 === 'yes' || response5 === 'y') {
@@ -52,3 +58,4 @@ if(response5 === 'yes' || response5 === 'y') {
   alert('Try to answer with a yes/y or no/n next time.');
 }
 console.log('Can Jennifer speak Mandarine? User answered: ' + response5);
+document.write('You answered: ' + response5 + ' to question #5');
